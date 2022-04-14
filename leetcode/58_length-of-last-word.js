93ef06b6c3cc7824b6d882ae89af712b2232bef0
Ohidul Islam <ohidulislam.me@gmail.com>
2022-04-15 00:20:36 +0600

    58 succeeded

---
+++ leetcode/58_length-of-last-word.js
@@ -0,0 +1,8 @@
+var lengthOfLastWord = function (s) {
+	const newStr = s.trim().split(" ");
+	// console.log(s)
+	return newStr[newStr.length - 1].length;
+};
+
+// console.log(lengthOfLastWord("Hello world"))
+// console.log(lengthOfLastWord("   fly me   to   the moon  "))
